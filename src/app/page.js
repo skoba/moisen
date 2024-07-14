7b1868e1e75b4e2f3dbc8175950d255354b75322
reodon <reodon@users.noreply.github.com>
2024-07-14 22:29:29 +0900

    ページのタイトルとディスクリプションを設定

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
+export const metadata = {
+  title: 'ホーム'
+};
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
